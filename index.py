ecb1b6e3a886414404e8fc8e6eabbda05ed8c4da
Lilya Benladjreb <lilyacbenladjreb@gmail.com>
2023-04-06 19:55:34 -0400

    faire A3

--- index.py
+++ index.py
@@ -37,21 +37,46 @@ def update_database():
     url = 'https://data.montreal.ca/dataset/05a9e718-6810-4e73-8bb9-5955efeb91a0/resource/7f939a08-be8a-45e1-b208-d8744dca8fc6/download/violations.csv'
     response = requests.get(url)
     data = response.content.decode('utf-8')
-    data = csv.reader(data.splitlines(), delimiter=',')
+    csv_reader = csv.DictReader(data.splitlines())
+    contrevenants = [dict(row) for row in csv_reader]
     conn = sqlite3.connect('db/db.db')
     cursor = conn.cursor()
-    for row in data:
+    for contrevenant in contrevenants:
         # Vérifier si la ligne existe déjà dans la base de données avant de l'insérer
-        cursor.execute('SELECT * FROM contrevenants WHERE id_poursuite = ?', (row[0],))
+        cursor.execute('SELECT * FROM contrevenants WHERE id_poursuite = ?', (contrevenant[0],))
         existing_row = cursor.fetchone()
         if existing_row is None:
-            cursor.execute('INSERT INTO contrevenants VALUES (?,?,?,?,?,?,?,?,?,?)', row)
+            cursor.execute("""
+                INSERT INTO contrevenants (
+                    id_poursuite,
+                    business_id,
+                    etablissement,
+                    categorie,
+                    adresse,
+                    description,
+                    proprietaire,
+                    date_infraction,
+                    date_jugement,
+                    montant
+                ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+            """, (
+                contrevenant['id_poursuite'],
+                contrevenant['business_id'],
+                contrevenant['etablissement'],
+                contrevenant['categorie'],
+                contrevenant['adresse'],
+                contrevenant['description'],
+                contrevenant['proprietaire'],
+                contrevenant['date'],
+                contrevenant['date_jugement'],
+                contrevenant['montant']
+            ))
     conn.commit()
     conn.close()
 
 # Configuration du BackgroundScheduler pour exécuter la fonction update_database() chaque jour à minuit
 scheduler = BackgroundScheduler()
-scheduler.add_job(func=update_database, trigger='interval', days=1, start_date='2023-04-07 00:00:00')
+scheduler.add_job(func=update_database, trigger="cron", hour=0, minute=0)
 scheduler.start()
 
 @app.teardown_appcontext
